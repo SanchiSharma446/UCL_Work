579532a9aeb3973fd137dc0375565109681bea05
Sanchi <112593732+SanchiManchi@users.noreply.github.com>
2024-10-06 18:20:03 +0100

    Create octogon.c

---
+++ COMP0002/octogon.c
@@ -0,0 +1,16 @@
+#include "graphics.h"
+#include "math.h"
+
+  int main(void)
+  {
+    setWindowSize(200,200);
+    drawLine(100,100,120,100);
+    drawLine(120,100,120+10*sqrt(2),100+10*sqrt(2));
+    drawLine(120+10*sqrt(2),100+10*sqrt(2),120+10*sqrt(2),100+20*sqrt(2));
+    drawLine(120,100+30*sqrt(2),120+10*sqrt(2),100+20*sqrt(2));
+    drawLine(100,100+30*sqrt(2),120,100+30*sqrt(2));
+    drawLine(100-10*sqrt(2),100+20*sqrt(2),100,100+30*sqrt(2));
+    drawLine(100-10*sqrt(2),100+20*sqrt(2),100-10*sqrt(2),100+10*sqrt(2));
+    drawLine(100,100,100-10*sqrt(2),100+10*sqrt(2));
+    return 0;
+}
