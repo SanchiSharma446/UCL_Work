263738c03f4e10b702a6cc2e7a12409dbfedf0cf
Sanchi <112593732+SanchiManchi@users.noreply.github.com>
2024-10-06 17:54:49 +0100

    Create triangle.c

---
+++ COMP0002/triangle.c
@@ -0,0 +1,10 @@
+#include "graphics.h"
+#include "stdio.h"
+
+
+int main(void) {
+    setWindowSize(300, 300);
+    drawLine(150,100,175,200);
+    drawLine(150,100,125,200);
+    drawLine(125,200,175,200);
+}
