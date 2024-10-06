be5b825dc682e6a5210acc7d4d37947b398145c2
Sanchi <112593732+SanchiManchi@users.noreply.github.com>
2024-10-06 19:33:43 +0100

    Create rects.c

---
+++ COMP0002/Worksheet1/rects.c
@@ -0,0 +1,15 @@
+#include "stdio.h"
+#include "graphics.h"
+#include "math.h"
+
+// Written by Sanchi 6th Oct 2024 // This program displays my name.
+
+int main(void)
+{
+  drawLine(30,30,120,30);
+  drawLine(120,30,120,80);
+  drawLine(120,80,30,80);
+  drawLine(30,30,30,80);
+
+  drawRect(150,150,60,140);
+  }
