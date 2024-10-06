90cdf535033bdb446be306aa7adaae1a848f5f63
Sanchi <112593732+SanchiManchi@users.noreply.github.com>
2024-10-06 20:41:23 +0100

    Create BT_Tower.c

---
+++ COMP0002/Worksheet1/BT_Tower.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+#include "graphics.h"
+
+int main(void)
+
+{
+  setWindowSize(1000,1000);
+  drawRect(50,50,75,25);
+  drawRect(25, 75, 125, 75);
+  drawRect(50,150,75,150);
+  drawRect(80,0,10,50);
+  }
