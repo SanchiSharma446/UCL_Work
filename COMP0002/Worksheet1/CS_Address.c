656b1d1eea64551d4ce88fe203ddfbbddb274776
Sanchi <112593732+SanchiManchi@users.noreply.github.com>
2024-10-08 14:13:10 +0100

    Create CS_Address.c

---
+++ COMP0002/Worksheet1/CS_Address.c
@@ -0,0 +1,9 @@
+#include "graphics.h"
+
+int main(void)
+{
+  drawString("Department of Computer Science", 100, 100);
+  drawString("University College London", 100, 120);
+  drawString("66-72 Gower Street", 100, 140);
+  drawString("London WC1E 6BT", 100, 160);
+}
