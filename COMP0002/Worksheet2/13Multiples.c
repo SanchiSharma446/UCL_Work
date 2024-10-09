84b0c5a01a391ae7916cf5386089d48e20b7db8f
Sanchi <112593732+SanchiManchi@users.noreply.github.com>
2024-10-09 13:30:31 +0100

    Update 13Multiples.c

--- COMP0002/Worksheet2/13Multiples.c
+++ COMP0002/Worksheet2/13Multiples.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 
+//with for
+
 int main()
 {
     for(int i = 0; i <=10; i++)
     {
         printf("%d * 13 = %d\n", i, i*13);
     }
+    return 0;
+}
+
+
+//with do
+
+int main()
+{
+    int i = 0;
+    do
+    {
+        printf("%d * 13 = %d\n", i, i*13);
+        i++;
+    }while(i < 11);
+    return 0;
 }
