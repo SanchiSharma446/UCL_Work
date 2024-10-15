d1d2e74d75dd325059f80324cf30f5fce1a5f564
Sanchi <112593732+SanchiManchi@users.noreply.github.com>
2024-10-15 09:14:28 +0100

    Update Powers.c

--- COMP0002/Worksheet3/Powers.c
+++ COMP0002/Worksheet3/Powers.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+//loop
+
 int power(int x, int y){
     int result = 1;
     for(int i = 0; i < y; i++){
@@ -8,6 +10,18 @@ int power(int x, int y){
     printf("%d\n", result);
     return result;
 }
+
+//recursion
+
+
+int powerrec(int x, int y){
+
+    if(y == 0)
+        return 1;
+    else
+        return x * power(x, y-1);
+}
+
 
 
 int main(){
